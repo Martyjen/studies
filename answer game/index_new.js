7bbb508301628f558f08147fe1e258ec46eb61b8
Martyjen <88323581+Martyjen@users.noreply.github.com>
2021-09-28 23:24:12 +0300

    master

---
+++ answer game/index_new.js
@@ -0,0 +1,11 @@
+var arreyResault = ["Да", "Нет", "Возможно"];
+var arreybagraundColor = ["bagraundGreen", "bagraundRed", "bagraundYellow"]
+
+document.getElementById("buttonId").addEventListener('click', function(){
+    var key = Math.floor(Math.random() * arreyResault.length); //Присваеваем переменной "key" случайное число которое не больше длины массива "arreyResault".
+    document.getElementById("select_block").className = arreybagraundColor[key]; 
+    document.getElementById("h2_id").innerHTML = arreyResault[key];
+});
+
+
+//Строка 5 - использовал длину массива "arreyResault.length" вместо зачения "3" - мне кажется, что так более универсальней. При добавлении в массив новых данных, не нужно будет менять значение "3"
