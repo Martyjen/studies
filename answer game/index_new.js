495f8d9e90180a85d4d9edde875de65af6b60ff3
Martyjen <88323581+Martyjen@users.noreply.github.com>
2021-10-03 22:04:30 +0300

    rotate background

--- answer game/index_new.js
+++ answer game/index_new.js
@@ -5,7 +5,7 @@ var r = 0;
 document.getElementById("buttonId").addEventListener('click', rotateBackground);
 
 function rotateBackground() {
-    document.getElementById("select_block").className = arreyData[1][i];
+    showResultAndColor(i);
     i++;
     if (i < arreyData[0].length) {
         setTimeout(rotateBackground, 100);
@@ -16,13 +16,14 @@ function rotateBackground() {
         setTimeout(rotateBackground, 100);
     }
     else {
-        resaultSelected();
         r = 0;
         i = 0;
+        var key = Math.floor(Math.random() * arreyData[0].length);
+        showResultAndColor(key);
     }
 }
 
-function resaultSelected() {
-    var key = Math.floor(Math.random() * arreyData[0].length); document.getElementById("h2_id").innerHTML = arreyData[0][key];
+function showResultAndColor(key) {
+    document.getElementById("h2_id").innerHTML = arreyData[0][key];
     document.getElementById("select_block").className = arreyData[1][key];
 }
