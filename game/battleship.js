1439739d66182bc046fd074ac2eafbb3bd0dca21
Martyjen <88323581+Martyjen@users.noreply.github.com>
2021-10-17 23:34:39 +0300

    master

--- game/battleship.js
+++ game/battleship.js
@@ -7,12 +7,12 @@ var view = {
 
 	displayHit: function(location){
 		var cell = document.getElementById(location);
-		cell.setAttribute("class", "miss");
+		cell.setAttribute("class", "hit");
 	},
 
 	displayMiss: function(location){
 		var cell = document.getElementById(location);
-		cell.setAttribute("class", "hit"); //Добавляем новый класс для переменной cell.
+		cell.setAttribute("class", "miss"); //Добавляем новый класс для переменной cell.
 	}
 }
 var model = {
@@ -20,28 +20,75 @@ var model = {
 	numShips: 3,
 	shipLength: 2,
 	shipsSunk: 0,
-
-ships = [
+	ships: [
 	{locations: ["10", "20", "30"], hits: ["", "", "hit"]},
 	{locations: ["32", "33", "34"], hits: ["", "hit", ""]},
 	{locations: ["63", "64", "65"], hits: ["", "", ""]}
 ],
-fire: function(){
+fire: function(guess){
 	for (var i = 0; i < this.numShips; i++){ // i - перебираем колиство масивов (караблей)
 		var ship = this.ships[i]; // this.ships[i] передаёт данные о количестве объектов в масиве [0-2] переменной "ship".
-		var index = ship.locations.indexOf(guess);
+		var index = ship.locations.indexOf(guess); //Найти в массиве "ship" значение переданное параметром "guess" и передать его переменной "index".
 		if (index >= 0){
 			ship.hits[index] = "hit";
-			if (this.shipsSunk(ship)){ 
+			view.displayHit(guess);
+			view.displayMassage("Попали!");
+			if (this.isSunk(ship)){ 
+				view.displayMassage("Вы потапили корабль!");
 				this.shipsSunk++; // Если значение массива
 			}
 			return true;
 		}
 		
 	}
+	view.displayMiss(guess);
+	view.displayMassage("Вы промахнулись");
 	return false;
 },
+isSunk: function(ship){
+	for (var i = 0; i , this.shipLength; i++){
+		if (ship.hits[i] !=="hit"){
+			return false;
+		}
+	}
+	return true;
+}
 };
+
+var controller = {
+	guesses: 0,
+	processGuess: function(guess){
+
+	}
+};
+
+function parseGuess(guess) {
+	var alphabet = ["A", "B", "C", "D", "F", "G"];
+	
+	if (guess === null || guess.length !==2){
+		alert ("Ошибка ввода координат!");
+	} else {
+		firstChar = guess.charAt(0);
+		var row = alphabet.indexOf(firstChar);
+		var column = guess.charAt(1);
+
+		if (isNan(row) || isNaN(column)) {
+			alert("вы ввели некорректные данные.");
+		
+		} else if (row < 0 || row >= model.boardSize ||
+			column < 0 || column >= model.boardSize){
+				alert ("Нет таких данных");
+			}
+				}
+
+		}
+
+// model.fire("53");
+// model.fire("06");
+// model.fire("14");
+// model.fire("33");
+// model.fire("32");
+// model.fire("22");
 
 
 
