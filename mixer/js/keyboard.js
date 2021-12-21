bc5150c04d8894a097b66348e052b4ce03f1d3db
Martyjen <88323581+Martyjen@users.noreply.github.com>
2021-12-21 23:42:20 +0300

    master

--- mixer/js/keyboard.js
+++ mixer/js/keyboard.js
@@ -35,18 +35,19 @@ function myFunction() {
 // let simvol = elNumber1.value.replace(/[0-9]/g, ''); //удалить все цивры, получиное передать переменной "simvol"
 
 function calculateResult() {
-    if (elNumber1.value.includes("-")) { // "includes" проверяет сроку на наличие "-", если он присуствует то "true"
-        let [box1, box3] = elNumber1.value.split("-"); //"split" разбивает строку на две отдельные переменных, "box1 и box3", делителем служет символ "-"
-        elNumber1.value = +box1 - +box3;
-    } else if (elNumber1.value.includes("+")) {
-        let [box1, box3] = elNumber1.value.split("+");
-        elNumber1.value = +box1 + +box3;
-    } else if (elNumber1.value.includes("/")) {
-        let [box1, box3] = elNumber1.value.split("/");
-        elNumber1.value = +box1 / +box3;
-    } else if (elNumber1.value.includes("*")) {
-        let [box1, box3] = elNumber1.value.split("*");
-            elNumber1.value = +box1 * +box3;
-    }
+    elNumber1.value = eval(elNumber1.value);
+    // if (elNumber1.value.includes("-")) { // "includes" проверяет сроку на наличие "-", если он присуствует то "true"
+    //     let [box1, box3] = elNumber1.value.split("-"); //"split" разбивает строку на две отдельные переменных, "box1 и box3", делителем служет символ "-"
+    //     elNumber1.value = +box1 - +box3;
+    // } else if (elNumber1.value.includes("+")) {
+    //     let [box1, box3] = elNumber1.value.split("+");
+    //     elNumber1.value = +box1 + +box3;
+    // } else if (elNumber1.value.includes("/")) {
+    //     let [box1, box3] = elNumber1.value.split("/");
+    //     elNumber1.value = +box1 / +box3;
+    // } else if (elNumber1.value.includes("*")) {
+    //     let [box1, box3] = elNumber1.value.split("*");
+    //         elNumber1.value = +box1 * +box3;
+    // }
 }
 
---
+++ test/index.js
@@ -0,0 +1,24 @@
+let tempestString = "-2+3-876-5";
+
+// for (let i = 0; i>xxx.){
+
+// }
+
+function splitString(stringToSplit, separator) {
+    var arrayOfStrings = stringToSplit.split(separator);
+  
+    console.log('Оригинальная строка: "' + stringToSplit + '"');
+    console.log('Разделитель: "' + separator + '"');
+    console.log('Массив содержит ' + arrayOfStrings.length + ' элементов: ' + arrayOfStrings.join(' / '));
+  }
+  
+  // Строчка из «Бури» Шекспира. Перевод Михаила Донского.
+//   var tempestString = 'И как хорош тот новый мир, где есть такие люди!';
+  var monthString = 'Янв,Фев,Мар,Апр,Май,Июн,Июл,Авг,Сен,Окт,Ноя,Дек';
+  
+  var space = '+';
+  var comma = ',';
+  
+  splitString(tempestString, space);
+  splitString(tempestString);
+  splitString(monthString, comma);
