eb9780ad48b4a4fa24fd084048735e826bddb43f
Martyjen <88323581+Martyjen@users.noreply.github.com>
2022-03-08 22:45:27 +0300

    master

--- mixer2/js/calculator.js
+++ mixer2/js/calculator.js
@@ -16,10 +16,10 @@ slyleInput(); // Загрузка стилей.
 
 // Оформление Input-ов.
 function slyleInput() {
-inputTwo.style.fontSize = 23 + "px";
-inputTwo.style.color = "red";
-inputThree.style.fontSize = 23 + "px";
-inputThree.style.color = "red";
+    inputTwo.style.fontSize = 23 + "px";
+    inputTwo.style.color = "red";
+    inputThree.style.fontSize = 23 + "px";
+    inputThree.style.color = "red";
 }
 
 //-== Варианты клавиатуры==-
@@ -51,11 +51,11 @@ function calculiator() {
             }
             if (event.path[0].value === "ce") {
                 inputOne.value = "";
-                }
+            }
             else if (event.path[0].value === "delite") {
                 oneDelSimbol(-1);
             }
-            else if (event.path[0].value == " -"){
+            else if (event.path[0].value == " -") {
                 modulMunus();
             }
             else if (event.path[0].value == "=") {
@@ -73,18 +73,20 @@ function calculiator() {
         });
     }
 
-    //модуль -
-       function modulMunus(){
-
-        if (inputOne.value[0] != "-"){
-      let arryInput = inputOne.value.split('');
-      arryInput.unshift("-");
-      inputOne.value = arryInput.join("");
-        } else if (inputOne.value[0] == "-"){
+    //модуль "минус" - временное решение.
+    function modulMunus() {
+        if (inputOne.value[0] == "-") {
             inputOne.value = inputOne.value.slice(1);
+        } else if (inputOne.value[1] == "-") {
+            inputOne.value = inputOne.value.slice(2);
+        } else if (inputOne.value[0] != "-") {
+            let arryInput = inputOne.value.split(''); // разбить строку на массив.
+            arryInput.unshift("-"); // добавить "-" в начало массива.
+            inputOne.value = arryInput.join(""); // все объекты массива объеденить в строку.
         }
-      }
+    }
 
+    //   inputOne.value = inputOne.value.trimStart();
     //Сравнение и вывод:
     function outputChange() {
         if (String(inputOne.value) === " Infinity" || String(inputOne.value) === " -Infinity") {
@@ -114,13 +116,13 @@ function calculiator() {
     }
 
     function addInput3() {
-        inputThree.value = inputTwo.value; 
+        inputThree.value = inputTwo.value;
         localStorage.setItem('calcInputThree', inputThree.value);
     }
 
     //Запрет ввода первым математических операторов + - * / и 00 .
     function noInput() {
-        for (let i = 0; i < 10; ++i) { 
+        for (let i = 0; i < 10; ++i) {
             if (inputOne.value == '+' || inputOne.value == "-" || inputOne.value == "/" || inputOne.value == "*" || inputOne.value == "0" + i || inputOne.value == " 0" + i) {
                 oneDelSimbol(-1);
             }
