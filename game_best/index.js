f8ac723d2523bbd3d3f2b7dee8e20a166e1554a6
Martyjen <88323581+Martyjen@users.noreply.github.com>
2022-03-22 23:22:00 +0300

    master

--- game_best/index.js
+++ game_best/index.js
@@ -9,35 +9,111 @@ let tankGame = {
   horisontalPosition: 0,
 }
 
+let tankGame2 = {
+  mapGame: document.querySelector(".game"),
+  mapVertecal: 600, // Вертикальный развер карты - кратное скорости танка.
+  mapHorizontal: 1200, // Горизантальный развер карты - кратное скорости танка.
+  tank: document.querySelector("#tank2"),
+  speedTank: 25, // Скорость танка - половина от размера танка.
+  tankSize: 50, // Размер танка.
+  verticalPosition: 0,
+  horisontalPosition: 1175,
+
+}
+
 //Инициализация игры.
 tankGame.mapGame.style.width = tankGame.mapHorizontal + tankGame.speedTank + "px";
 tankGame.mapGame.style.height = tankGame.mapVertecal + tankGame.speedTank + "px";
 tankGame.tank.style.width = tankGame.tankSize + "px";
 tankGame.tank.style.height = tankGame.tankSize + "px";
+tankGame.tank.style.left = tankGame.verticalPosition + "px";
+tankGame.tank.style.top = tankGame.horisontalPosition + "px";
 
-document.addEventListener('keydown', function (event) {
 
-  if (event.key == "ArrowDown"){
-    tankGame.verticalPosition = tankGame.verticalPosition + tankGame.speedTank;
-    tank.style.top = tankGame.verticalPosition + "px";
-    console.log (tankGame.verticalPosition);
-  }
-  if (event.key == "ArrowUp"){
+tankGame2.tank.style.width = tankGame2.tankSize + "px";
+tankGame2.tank.style.height = tankGame2.tankSize + "px";
+tankGame2.tank.style.left = tankGame2.horisontalPosition + "px";
+tankGame2.tank.style.top = tankGame2.verticalPosition + "px";
+
+// document.addEventListener('keydown', function (event) {
+
+  // if (event.key == "ArrowDown"){
+  //   tankGame.verticalPosition = tankGame.verticalPosition + tankGame.speedTank;
+  //   tank.style.top = tankGame.verticalPosition + "px";
+  //   console.log (tankGame.verticalPosition);
+  // }
+  // if (event.key == "ArrowUp"){
+  //   tankGame.verticalPosition = tankGame.verticalPosition - tankGame.speedTank;
+  //   tank.style.top = tankGame.verticalPosition + "px";
+  //   console.log (tankGame.verticalPosition);
+  // }
+  // if (event.key == "ArrowLeft"){
+  //   tankGame.horisontalPosition = tankGame.horisontalPosition - tankGame.speedTank;
+  //   tank.style.left = tankGame.horisontalPosition + "px";
+  //   console.log (tankGame.horisontalPosition);
+  // }
+  // if (event.key == "ArrowRight"){
+  //   tankGame.horisontalPosition = tankGame.horisontalPosition + tankGame.speedTank;
+  //   tank.style.left = tankGame.horisontalPosition + "px";
+  //   console.log (tankGame.horisontalPosition);
+  // }
+
+  //---------------------------------------------------------------------
+  let i = 0;
+  while (i < 1){
+      ++i;
+      setInterval(randomF, 100)
+ 
+}
+function randomF(){
+  let rendome = (Math.floor(Math.random() * 4));
+
+if (rendome === 1 ) {
+  up();
+}
+if (rendome === 2) {
+  down();
+}
+if (rendome === 3) {
+  left()
+}
+if (rendome === 0) {
+  right();
+}
+}
+
+  function up() {
     tankGame.verticalPosition = tankGame.verticalPosition - tankGame.speedTank;
     tank.style.top = tankGame.verticalPosition + "px";
-    console.log (tankGame.verticalPosition);
-  }
-  if (event.key == "ArrowLeft"){
-    tankGame.horisontalPosition = tankGame.horisontalPosition - tankGame.speedTank;
-    tank.style.left = tankGame.horisontalPosition + "px";
-    console.log (tankGame.horisontalPosition);
-  }
-  if (event.key == "ArrowRight"){
-    tankGame.horisontalPosition = tankGame.horisontalPosition + tankGame.speedTank;
-    tank.style.left = tankGame.horisontalPosition + "px";
-    console.log (tankGame.horisontalPosition);
+    console.log(tankGame.verticalPosition);
+    blockMap();
   }
 
+  function down() {
+    tankGame.verticalPosition = tankGame.verticalPosition + tankGame.speedTank;
+    tank.style.top = tankGame.verticalPosition + "px";
+    console.log(tankGame.verticalPosition);
+    blockMap();
+  }
+
+  function right() {
+    tankGame.horisontalPosition = tankGame.horisontalPosition + tankGame.speedTank;
+    tank.style.left = tankGame.horisontalPosition + "px";
+    console.log(tankGame.horisontalPosition);
+    blockMap();
+  }
+
+  function left() {
+    tankGame.horisontalPosition = tankGame.horisontalPosition - tankGame.speedTank;
+    tank.style.left = tankGame.horisontalPosition + "px";
+    console.log(tankGame.horisontalPosition);
+    blockMap();
+  }
+
+  //===================================================================================
+  
+  function blockMap() {
+  
   if (tankGame.horisontalPosition <= 0) {
     tankGame.horisontalPosition = 0;
     tank.style.left = 0 + "px";
@@ -52,7 +128,55 @@ document.addEventListener('keydown', function (event) {
   }
   if (tankGame.verticalPosition >= tankGame.mapVertecal) {
     tankGame.verticalPosition = tankGame.mapVertecal - tankGame.speedTank;
-    tank.style.top = tankGame.mapVertecal -  tankGame.tankSize + tankGame.speedTank + "px";
-  
+    tank.style.top = tankGame.mapVertecal - tankGame.tankSize + tankGame.speedTank + "px";
+  }
 }
-});
+// });
+
+//=======================================================================================================
+
+document.addEventListener('keydown', function (event) {
+
+  if (event.key == "ArrowDown"){
+    tankGame2.verticalPosition = tankGame2.verticalPosition + tankGame2.speedTank;
+    tank2.style.top = tankGame2.verticalPosition + "px";
+    console.log (tankGame2.verticalPosition);
+   
+  }
+  if (event.key == "ArrowUp"){
+    tankGame2.verticalPosition = tankGame2.verticalPosition - tankGame2.speedTank;
+    tank2.style.top = tankGame2.verticalPosition + "px";
+    console.log (tankGame2.verticalPosition);
+    
+  }
+  if (event.key == "ArrowLeft"){
+    tankGame2.horisontalPosition = tankGame2.horisontalPosition - tankGame2.speedTank;
+    tank2.style.left = tankGame2.horisontalPosition + "px";
+    console.log (tankGame2.horisontalPosition);
+    
+  }
+  if (event.key == "ArrowRight"){
+    tankGame2.horisontalPosition = tankGame2.horisontalPosition + tankGame2.speedTank;
+    tank2.style.left = tankGame2.horisontalPosition + "px";
+    console.log (tankGame2.horisontalPosition);
+   
+  }
+  
+    if (tankGame2.horisontalPosition <= 0) {
+      tankGame2.horisontalPosition = 0;
+      tank2.style.left = 0 + "px";
+    }
+    if (tankGame2.horisontalPosition >= tankGame2.mapHorizontal) {
+      tankGame2.horisontalPosition = tankGame2.mapHorizontal - tankGame2.speedTank;
+      tank2.style.left = tankGame2.mapHorizontal - tankGame2.tankSize + tankGame2.speedTank + "px";
+    }
+    if (tankGame2.verticalPosition <= 0) {
+      tankGame2.verticalPosition = 0;
+      tank2.style.top = 0 + "px";
+    }
+    if (tankGame2.verticalPosition >= tankGame2.mapVertecal) {
+      tankGame2.verticalPosition = tankGame2.mapVertecal - tankGame2.speedTank;
+      tank2.style.top = tankGame2.mapVertecal - tankGame2.tankSize + tankGame2.speedTank + "px";
+    }
+  
+  });
