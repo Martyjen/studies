6c0ff9a49dbbe34f6a9bad8f50d69e94c863ded0
Martyjen <88323581+Martyjen@users.noreply.github.com>
2022-11-06 21:47:14 +0300

    master

--- test_project/mixer2/js/display.js
+++ test_project/mixer2/js/display.js
@@ -1,4 +1,5 @@
-const displayInput = `
+const displayInput = 
+`
 <input class="color1 input-class" type="text" readonly id="inputOne" onkeyup="myFunction()">
 <input class="color2 border-top input-class" type="text" readonly id="inputTwo">
 <input class="color3 border-top input-class" type="text" readonly id="inputThree">
